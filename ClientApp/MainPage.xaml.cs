7473eb19607cdd9e65ffcc7ceb4ea61f79dbaae0
Martin <Martin@MARTINPC>
2016-11-22 17:37:29 +0100

    Agregar archivos de proyecto.

---
+++ ClientApp/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.AppService;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private AppServiceConnection inventoryService;
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Add the connection.
+            if (this.inventoryService == null)
+            {
+                this.inventoryService = new AppServiceConnection();
+
+                // Here, we use the app service name defined in the app service provider's Package.appxmanifest file in the <Extension> section.
+                this.inventoryService.AppServiceName = "com.microsoft.inventory";
+
+                // Use Windows.ApplicationModel.Package.Current.Id.FamilyName within the app service provider to get this value.
+                this.inventoryService.PackageFamilyName = "b29d24f1-b70e-416d-a9c3-36457ec83ccc_n6fhnfby9ccnm";
+
+                var status = await this.inventoryService.OpenAsync();
+                if (status != AppServiceConnectionStatus.Success)
+                {
+                    button.Content = "Failed to connect";
+                    return;
+                }
+            }
+
+            // Call the service.
+            int idx = int.Parse(textBox.Text);
+            var message = new ValueSet();
+            message.Add("Command", "Item");
+            message.Add("ID", idx);
+            AppServiceResponse response = await this.inventoryService.SendMessageAsync(message);
+            string result = "";
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data  that the service sent  to us.
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result = response.Message["Result"] as string;
+                }
+            }
+
+            message.Clear();
+            message.Add("Command", "Price");
+            message.Add("ID", idx);
+            response = await this.inventoryService.SendMessageAsync(message);
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data that the service sent to us.
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result += " : Price = " + response.Message["Result"] as string;
+                }
+            }
+
+            button.Content = result;
+
+        }
+    }
+}
